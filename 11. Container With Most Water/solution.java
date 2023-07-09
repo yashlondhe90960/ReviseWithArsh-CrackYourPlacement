f5319c05e2eb26f557dbf41b27a143f7cf75f14a
Yash Londhe <99034957+yashlondhe90960@users.noreply.github.com>
2023-07-10 02:41:15 +0530

    Day 4 completed

--- 11. Container With Most Water/solution.java
+++ 11. Container With Most Water/solution.java
@@ -8,25 +8,22 @@ import java.util.Map;
 
 
 class Solution {
-    public int subarraysDivByK(int[] nums, int k) {
-        int[] prefixSums = new int[nums.length+1];
-        for(int i=1;i<= nums.length;i++){
-            prefixSums[i] = prefixSums[i-1]+nums[i-1];
+    public int maxArea(int[] height) {
+        int left = 0;
+        int right = height.length - 1;
+        int maxArea = 0;
+
+        while(left < right){
+            int area = Math.min(height[left], height[right])*(right - left);
+
+            maxArea = Math.max(area,maxArea);
+            if(height[left] < height[right]){
+                left++;
+            } else right--;
 
         }
-        int[] remainders = new int[k];
-        int count =0 ;
-        for(int i=0;i<= nums.length;i++){
-            int remainder = prefixSums[i] % k;
-            if(remainder < 0){
-                remainder += k;
-
-            }
-            count += remainders[remainder];
-            remainders[remainder]++;
-
-        }
-        return count;
+        return maxArea;
+        
 
     }
 }
