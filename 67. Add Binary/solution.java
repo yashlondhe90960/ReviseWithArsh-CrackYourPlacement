3c11cdd3b96a673955967ff70eba8297c26c7d68
yashlondhe90960 <yashlondhe9151@gmail.com>
2023-07-24 00:21:56 +0530

    Day 18 completed

--- 67. Add Binary/solution.java
+++ 67. Add Binary/solution.java
@@ -4,7 +4,51 @@ import java.util.Arrays;
  * @author : yashlondhe90960
  * 
  */
+//m1
 
+class Solution {
+    public String addBinary(String a, String b) {
+        char carry = '0';
+        StringBuilder sb = new StringBuilder();
+        char[] aCh = a.toCharArray();
+        char[] bCh = b.toCharArray();
+        int i = aCh.length-1;
+        int j = bCh.length-1;
+        while(i >= 0 || j >= 0) {
+            char tempA = i >= 0 ? aCh[i] : '0';
+            char tempB = j >= 0 ? bCh[j] : '0'; 
+            if(tempA == tempB) {
+                if(carry == '0') {
+                    sb.append("0"); 
+                }
+                else {
+                    sb.append('1');
+                }
+                carry = tempA;
+            }
+            else if(tempA != tempB) {
+                if(carry == '0') {
+                    sb.append("1");
+                }
+                else {
+                    sb.append("0");
+                    carry = '1';
+                }
+            }
+            i--;
+            j--;
+        }
+        if(carry == '1')
+            sb.append("1");
+        sb.reverse();
+        return sb.toString();
+    }
+}
+
+
+
+
+//m2
 class Solution {
     public String addBinary(String a, String b) {
         StringBuilder res = new StringBuilder();
