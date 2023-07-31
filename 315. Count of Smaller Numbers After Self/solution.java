c302eddf4f2488efbebfb489125ea71d0f385221
yashlondhe90960 <yashlondhe9151@gmail.com>
2023-07-31 23:37:56 +0530

    Day 26 Completed!

--- 315. Count of Smaller Numbers After Self/solution.java
+++ 315. Count of Smaller Numbers After Self/solution.java
@@ -7,35 +7,48 @@ import java.util.Arrays;
 
 
 public class Solution {
-    class Node {
-        Node left, right;
-        int val, sum, dup = 1;
-        public Node(int v, int s) {
-            val = v;
-            sum = s;
-        }
-    }
-    public List<Integer> countSmaller(int[] nums) {
-        Integer[] ans = new Integer[nums.length];
-        Node root = null;
-        for (int i = nums.length - 1; i >= 0; i--) {
-            root = insert(nums[i], root, ans, i, 0);
-        }
-        return Arrays.asList(ans);
-    }
-    private Node insert(int num, Node node, Integer[] ans, int i, int preSum) {
-        if (node == null) {
-            node = new Node(num, 0);
-            ans[i] = preSum;
-        } else if (node.val == num) {
-            node.dup++;
-            ans[i] = preSum + node.sum;
-        } else if (node.val > num) {
-            node.sum++;
-            node.left = insert(num, node.left, ans, i, preSum);
-        } else {
-            node.right = insert(num, node.right, ans, i, preSum + node.dup + node.sum);
-        }
-        return node;
-    }
+	public List<Integer> countSmaller(int[] nums) {
+		List<Integer> res = new ArrayList<>();
+		if(nums == null || nums.length == 0) return res;
+		TreeNode root = new TreeNode(nums[nums.length - 1]);
+		res.add(0);
+		for(int i = nums.length - 2; i >= 0; i--) {
+			int count = insertNode(root, nums[i]);
+			res.add(count);
+		}
+		Collections.reverse(res);
+		return res;
+	}
+
+	public int insertNode(TreeNode root, int val) {
+		int thisCount = 0;
+		while(true) {
+			if(val <= root.val) {
+				root.count++;
+				if(root.left == null) {
+					root.left = new TreeNode(val); break;
+				} else {
+					root = root.left;
+				}
+			} else {
+				thisCount += root.count;
+				if(root.right == null) {
+					root.right = new TreeNode(val); break;
+				} else {
+					root = root.right;
+				}
+			}
+		}
+		return thisCount;
+	}
+}
+
+class TreeNode {
+	TreeNode left; 
+	TreeNode right;
+	int val;
+	int count = 1;
+	public TreeNode(int val) {
+		this.val = val;
+	}
 }
